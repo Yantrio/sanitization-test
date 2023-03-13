0da3d7cc4a880d1f67be87c6f67b9bc41fe21e15
James Humphries <jamesh@spacelift.io>
2023-03-13 10:04:14 +0000

    Adding some new nested outputs and resources

--- main.tf
+++ main.tf
@@ -58,3 +58,63 @@ output "non_sensitive_variable_output" {
   value     = var.non_sensitive_variable
   sensitive = false
 }
+
+resource "random_integer" "random_id" {
+  min = 100
+  max = 999
+}
+
+resource "random_string" "random_name" {
+  length  = 8
+  special = false
+}
+
+resource "null_resource" "nested_resource" {
+  provisioner "local-exec" {
+    command = "echo 'Hello, World!'"
+  }
+}
+
+resource "local_file" "example_file" {
+  content  = "This is some example content"
+  filename = "example.txt"
+}
+
+resource "null_resource" "file_permission" {
+  triggers = {
+    file_contents = local_file.example_file.content
+  }
+
+  provisioner "local-exec" {
+    command = "chmod 644 ${local_file.example_file.filename}"
+  }
+}
+
+output "nested_resources" {
+  value = {
+    random_integer = {
+      id    = random_integer.random_id.id
+      value = random_integer.random_id.result
+    }
+    random_string = {
+      id    = random_string.random_name.id
+      value = random_string.random_name.result
+      nested_resources = {
+        null_resource = {
+          triggers = null_resource.nested_resource.triggers
+          nested_resources = {
+            local_file = {
+              filename = local_file.example_file.filename
+              content  = local_file.example_file.content
+              nested_resources = {
+                null_resource = {
+                  triggers = null_resource.file_permission.triggers
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
